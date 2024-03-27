876887667d4fd7707d030ffcc2e236968f0f48c8
sakitamako <sa.919.1025.ki@gmail.com>
2024-03-27 21:38:38 +0900

    インデント編集

--- rensyu6/src/rensyu6/sakitamako/com/github/SetSample6.java
+++ rensyu6/src/rensyu6/sakitamako/com/github/SetSample6.java
@@ -6,16 +6,16 @@ import java.util.Set;
 public class SetSample6 {
 	public static void main(String[] args) {
         Set<String> set = new HashSet<String>();
-
+        
         //値のセットは add メソッドを利用
         set.add("value1");
         set.add("value2");
         set.add("value3");
         set.add("value4");
         set.add("value5");
-
+        
         set.add("value2"); //重複要素を add
-
+        
         //拡張 For 文を利用
         for (String s: set) {
         	System.out.println(s);
